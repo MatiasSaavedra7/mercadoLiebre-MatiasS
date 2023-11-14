8d270ddf19632a0e59cda42fd703b2cd29af11d8
matiases1995 <matiassaavedra1995@gmail.com>
2023-11-13 21:40:47 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const { log } = require('console');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const publicPathFold = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPathFold));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+});
+
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('views/login.html'))
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port} - http://localhost:${port}/`)
+});
